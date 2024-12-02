9e6d17dd658637a3a2f0822acb5620b0fd005775
Seif Mostafa <seif.hussam.96@gmail.com>
2024-12-02 00:12:02 +0000

    chore: add completetions to  LLMGaurd

--- llm_guard/llm_guard/llm_guard.py
+++ llm_guard/llm_guard/llm_guard.py
@@ -2,8 +2,8 @@
 It will contain the main class that will be used to create the LLMGuard object.
 """
 
-from litellm.main import Completions
-from litellm.types.utils import ModelResponse
+from litellm import completion as llm_completion
+from litellm.types.utils import ModelResponse, Choices, Message
 
 from typing import Callable, Any
 
@@ -16,29 +16,46 @@ def default_output_guard(_messages) -> bool:
     return True
 
 
+def create_invalid_response(text: str) -> ModelResponse:
+    return ModelResponse(
+        id="",
+        object="chat.completion",
+        created=0,
+        model="",
+        choices=[
+            Choices(
+                index=0,
+                finish_reason="length",
+                message=Message(
+                    content=text,
+                    role="assistant",
+                ),
+            )
+        ],
+    )
+
+
 class LLMGaurd:
     def __init__(
         self,
-        completions: Completions,
         input_guard: Callable[[Any], bool] = None,
         output_guard: Callable[[Any], bool] = None,
         invalid_input_response: ModelResponse = None,
         invalid_output_response: ModelResponse = None,
     ):
-        self.completions = completions
         self.input_guard = input_guard if input_guard else default_input_guard
         self.output_guard = output_guard if output_guard else default_output_guard
         self.invalid_input_response = invalid_input_response
         self.invalid_output_response = invalid_output_response
 
-    def create(self, **kwargs):
+    def completions(self, **kwargs):
         """create a completion using the completions resource object."""
         messages = kwargs.get("messages")
 
         if not self.input_guard(messages):
             return self.invalid_input_response
 
-        output_response = self.completions.create(**kwargs)
+        output_response = llm_completion(**kwargs)
 
         if not self.output_guard(output_response):
             return self.invalid_output_response
